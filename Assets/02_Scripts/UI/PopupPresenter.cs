99dc35c579c0ca15ae49a91c0c9830cac90075de
Doğukan Karabıyık <dokarabiyik2008@gmail.com>
2023-06-19 13:48:36 +0300

    Changed return type of a function to make the code more cleanier

--- Assets/02_Scripts/UI/PopupPresenter.cs
+++ Assets/02_Scripts/UI/PopupPresenter.cs
@@ -13,7 +13,7 @@ namespace GemTrader.UI
         private GridLayoutGroup _container;
         private GemController _gemController;
         private GemTypeDataHolder _gemTypeDataHolder;
-        private GameObject[] _createdContainerItems;
+        private GameObject[] _ContainerItems;
 
         private void Awake()
         {
@@ -24,34 +24,32 @@ namespace GemTrader.UI
 
         private void Start()
         {
-            _createdContainerItems = InstantiateContainerItems();
+            InstantiateContainerItems();
             gameObject.SetActive(false);
         }
 
-        private GameObject[] InstantiateContainerItems()
+        private void InstantiateContainerItems()
         {
-            GameObject[] containerItems = new GameObject[_gemTypeDataHolder.gems.Length];
+            _ContainerItems = new GameObject[_gemTypeDataHolder.gems.Length];
 
             for (int i = 0; i < _gemTypeDataHolder.gems.Length; i++)
             {
                 GameObject containerItem = Instantiate(containerItemTemplate.gameObject, _container.transform);
-                containerItems[i] = containerItem;
-                
+                _ContainerItems[i] = containerItem;
+
                 containerItem.transform.GetChild(0).GetComponent<Image>().sprite = _gemTypeDataHolder.gems[i].Icon;
                 containerItem.transform.GetChild(1).GetComponent<TextMeshProUGUI>()
                     .text = _gemTypeDataHolder.gems[i].GemName;
             }
-
-            return containerItems;
         }
 
         private void HandlePopupText()
         {
-            for (int i = 0; i < _createdContainerItems.Length; i++)
+            for (int i = 0; i < _ContainerItems.Length; i++)
             {
                 if (_gemController.GemCountDict.TryGetValue(_gemTypeDataHolder.gems[i].GemName, out int value))
                 {
-                    _createdContainerItems[i].transform.GetChild(2).GetComponent<TextMeshProUGUI>()
+                    _ContainerItems[i].transform.GetChild(2).GetComponent<TextMeshProUGUI>()
                         .text = $"Collected Sum: {value}";
                 }
             }
